417e5a656614f12ab7d8905c30db8e4158d18cd2
Wagner Oliveira <waggner.dev@gmail.com>
2023-11-13 17:45:57 -0300

    Criação de Questions

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Question;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $questions = Question::paginate(15);
+        foreach ($questions as $question)
+        $options = json_decode($question->options, true);
+
+        return view('Admin.cruds.question.index', [
+            'questions'=>$questions,
+            'options'=>$options
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('Admin.cruds.question.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        try {
+            DB::beginTransaction();
+                if ($question = Question::create($data)){
+                    $question->question_text = $request->input('question_text');
+                    $question->options = json_encode($request->input('options'));
+                    $question->save();
+                    Session::flash('success', 'Avaliação cadastrada com sucesso!');
+                }
+            DB::commit();
+            return redirect()->route('admin.dashboard.question.index');
+        }catch (\Exception $exception){
+            dd($exception);
+            DB::rollBack();
+            Session::flash('error', 'Erro ao cadastrar avaliação');
+            return redirect()->back();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Question $question)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Question $question)
+    {
+        $question = Question::findOrFail($question->id);
+        $options = json_decode($question->options, true);
+//        dd($question);
+        return view('Admin.cruds.question.edit', [
+            'question'=>$question,
+            'options'=>$options
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Question $question)
+    {
+        $data = $request->all();
+
+        try {
+            DB::beginTransaction();
+                $question->fill([
+                    'question_text' => $data['question_text'],
+                ])->save();
+
+
+                $options = json_decode($data['options'], true);
+                $question->options = $options;
+                $question->save();
+            DB::commit();
+            Session::flash('success', 'Avaliação atualizada com sucesso!');
+            return redirect()->route('admin.dashboard.question.index');
+        }catch (\Exception $exception){
+            DB::rollBack();
+            Session::flash('error', 'Erro ao atualizar avaliação');
+            return redirect()->back();
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Question $question)
+    {
+        $question->delete();
+        Session::flash('success','Avaliação deletada com sucesso!');
+        return redirect()->back();
+    }
+
+    public function checkAnswer(Request $request)
+    {
+        $questionId = $request->input('question_id');
+        $selectedOption = $request->input('selected_option');
+
+        $question = Question::find($questionId);
+
+        if ($question->correct_option == $selectedOption) {
+            return response()->json(['correct' => true]);
+        } else {
+            return response()->json(['correct' => false]);
+        }
+    }
+}
