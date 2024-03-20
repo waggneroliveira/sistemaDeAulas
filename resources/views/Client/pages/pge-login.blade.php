ba44bf412a00c40a7a19811f3d738cea17c06fb4
DekiWill <William_Watanabe@hotmail.com>
2024-03-20 20:09:55 -0300

    Update: Login-page

--- resources/views/Client/pages/pge-login.blade.php
+++ resources/views/Client/pages/pge-login.blade.php
@@ -1,30 +1,46 @@
 @extends('Client.core.LAYOUTS.main')
 @section('content')
+<style>
+  .mdl-header,
+  .mdl-footer{
+    display: none;
+  }
+</style>
 <section class="pge-login">
   <main class="pge-login__main">
 
-    <form action="" class="pge-login__form">
+    <form action="" class="pge-login__main__form">
 
-      <img src="" alt="" class="pge-login__form">
+      <img src="{{ asset('Client/assets/images/logo-footer.svg') }}" alt="Logo da WHI" class="pge-login__main__form__logo">
 
       @include('Client.components.inputs.cmp-inp-email', [
           'placeholder' => 'Email',
           'name' => 'email',
           'required' => false,
-          'value'=> ''
       ])
 
       @include('Client.components.inputs.cmp-inp-password', [
           'placeholder' => 'Senha',
           'name' => 'senha',
           'required' => false,
-          'value'=> ''
+      ])
+
+      <label class="pge-login__main__form__keep-connected">
+        <input type="checkbox" name="keep-connected" class='pge-login__main__form__keep-connected__input'> Matenha-me conectado
+      </label>
+
+      {{-- TODO: VER COM BACKEND PRA ONDE ESSE LINK LEVA --}}
+      <a href="#" class="pge-login__main__form__forgot-password">Esqueci minha senha</a>
+
+      @include('Client.components.buttons.cmp-btn-03', [
+        'type' => 'submit',
+        'text' => 'Entrar'
       ])
 
     </form>
 
-  </main>
+    <img src="{{ asset('Client/assets/images/pge-login__banner.png') }}" alt="BACKEND VER POSSIBILIDADE DE PEGAR DESCRIÇÃO NO PAINEL" class="pge-login__main__banner">
 
-  <img src="" alt="" class="pge-login__banner">
+  </main>
 </section>
 @endsection
